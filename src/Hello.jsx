8befb7e1b16bf0d132a74fb53f99d5fd81a9aaf9
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-08-30 19:51:01 -0400

    v1.0.1

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import Resources from './Resources'
 
-export default function App() {
+export default function Hello() {
   return (
     <main>
-      <a href="https://urwebsite">urname: </a>
-      <br></br>
-      I like to make <a href="https://nodejs.dev">Games</a>
+      <div className="hello">
+      <span class="hello-h">
+      <a href="https://github.com/amukh1"> Amukh1: </a>
+      <br />
+        I'm the guy
+      <br />
+       <span> to <em>build </em>  your </span>
+        <a href="https://nodejs.dev">Backend</a></span>
+
+      <Resources />
+      </div>
     </main>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Hello from './Hello'
+import Contact from './contact'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+
     <Hello />
-		<App />
+
+	<Contact />
+
+	<App />
+
 	</React.StrictMode>
 )
